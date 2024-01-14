218755b839a81ef3c1779cf1998e232279a4b0ce
Daniel Kamenov <sorostmcoc@gmail.com>
2024-01-14 22:12:00 +0200

    Create tokenTracker.js

---
+++ main/tokenTracker.js
@@ -0,0 +1,64 @@
+const { Connection, PublicKey } = require("@solana/web3.js");
+
+const RAYDIUM_PUBLIC_KEY = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
+
+const raydium = new PublicKey(RAYDIUM_PUBLIC_KEY);
+
+const connection = new Connection(`https://solana-mainnet.core.chainstack.com/0683a9bd3e0f806fd79f87bbc1a595fb`, {
+    wsEndpoint: "wss://solana-mainnet.core.chainstack.com/ws/0683a9bd3e0f806fd79f87bbc1a595fb"
+});
+
+// Monitor logs
+async function main(connection, programAddress) {
+    console.log("Monitoring logs for program:", programAddress.toString());
+    connection.onLogs(
+        programAddress,
+        ({ logs, err, signature }) => {
+            if (err) return;
+
+            if (logs && logs.some(log => log.includes("initialize2"))) {
+                console.log("Signature for 'initialize2':", signature);
+                fetchRaydiumAccounts(signature, connection);
+            }
+        },
+        "finalized"
+    );
+}
+
+// Parse transaction and filter data
+async function fetchRaydiumAccounts(txId, connection) {
+    const tx = await connection.getParsedTransaction(
+        txId,
+        {
+            maxSupportedTransactionVersion: 0,
+            commitment: 'confirmed'
+        });
+    
+    
+    const accounts = tx?.transaction.message.instructions.find(ix => ix.programId.toBase58() === RAYDIUM_PUBLIC_KEY).accounts;
+
+    if (!accounts) {
+        console.log("No accounts found in the transaction.");
+        return;
+    }
+
+    const tokenAIndex = 8;
+    const tokenBIndex = 9;
+
+    const tokenAAccount = accounts[tokenAIndex];
+    const tokenBAccount = accounts[tokenBIndex];
+
+    const displayData = [
+        { "Token": "A", "Account Public Key": tokenAAccount.toBase58() },
+        { "Token": "B", "Account Public Key": tokenBAccount.toBase58() }
+    ];
+    console.log("New LP Found");
+    console.log(generateExplorerUrl(txId));
+    console.table(displayData);
+}
+
+function generateExplorerUrl(txId) {
+    return `https://solscan.io/tx/${txId}`;
+}
+
+main(connection, raydium).catch(console.error);
